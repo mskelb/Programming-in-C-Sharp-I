cff1b30215225bc790969ef98c7f39d3e3f228b3
mskelb <molly.brandt09@gmail.com>
2023-11-06 09:03:34 +0100

    Update

--- Assignment 5/customerManager.cs
+++ Assignment 5/customerManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Assignment5
 {
-    internal class customerManager
+    internal class CustomerManager
     {
         List<Customer> customers;
         int maxNumberOfCustomers; 
-        public customerManager(int value)
+        public CustomerManager(int value)
         {
             // Creating a list of customers
             customers = new List<Customer>();
